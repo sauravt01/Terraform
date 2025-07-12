feb9a9a351ad43c836df1d2636fa73561aadc5f3
Saurav <sauravtripathi23@gmail.com>
2025-07-12 16:43:18 +0530

    Terrafom Security Scan code

---
+++ terraform_Security_Scan/tflint/main.tf
@@ -0,0 +1,9 @@
+resource "azurerm_storage_account" "example" {
+  name                     = var.storage_account_name
+  resource_group_name      = azurerm_resource_group.example.name
+  location                 = azurerm_resource_group.example.location
+  account_tier             = var.account_tier
+  account_replication_type = var.account_replication_type
+  public_network_access_enabled = false
+
+}
---
+++ terraform_Security_Scan/tflint/terraform.tfvars
@@ -0,0 +1,5 @@
+storage_account_name = "st8234"
+resource_group_name = "example-resource-group"
+location = "East US"
+account_tier = "Standard"
+account_replication_type = "GRS"
