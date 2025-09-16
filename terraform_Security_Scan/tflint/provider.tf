f00dc0c97587e1e914f434e77811c595974b640c
Saurav Tripathi <70850137+sauravt01@users.noreply.github.com>
2025-09-16 23:57:55 +0530

    Delete terraform_Security_Scan/tflint directory

--- terraform_Security_Scan/tflint/provider.tf
+++
@@ -1,7 +0,0 @@
-provider "azurerm" {
-  features {}
-
-  # Optional: Uncomment and set the following if using a specific subscription or tenant
-  # subscription_id = "your_subscription_id"
-  # tenant_id       = "your_tenant_id"
-}
