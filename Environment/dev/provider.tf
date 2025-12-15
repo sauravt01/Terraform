bf9a23571524dbf65164aaf6d776264e8166738a
Saurav <sauravtripathi23@gmail.com>
2025-12-15 23:58:13 +0530

    added remote backend

--- Environment/dev/provider.tf
+++ Environment/dev/provider.tf
@@ -6,14 +6,14 @@ terraform {
     }
   }
 
-  # backend "azurerm" {
-  #   resource_group_name  = "rg1234"
-  #   storage_account_name = "storageaccount9454"
-  #   container_name       = "tfstate"
-  #   key                  = "terraform-dev-tfstate"
-  # }
+  backend "azurerm" {
+    resource_group_name  = "rg1234"
+    storage_account_name = "storageaccount9454"
+    container_name       = "tfstate"
+    key                  = "terraform-dev-tfstate"
+  }
 
-  backend "local" {}
+  
 }
 
 
